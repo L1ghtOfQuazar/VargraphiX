481faaf45325bb02fcf166b2995e664ecb5af9db
DmitriyMaiorov <dimaletspalys@gmail.com>
2024-10-23 12:27:20 +0300

    Update plot.py
    add plot3d
    add plot_bar
    add plot_pie

--- plot.py
+++ plot.py
@@ -12,6 +12,35 @@ def plot2d(x, y):
     f.close()
     os.popen("octave plot2d.m")
 
+def plot3d(x, y, z):
+    x = [float(i) for i in x]
+    y = [float(i) for i in y]
+    z = [float(i) for i in z]
+    f = open("plot3d.m", "w+")
+    f.write(f'plot3({x}, {y}, {z})\n')
+    f.write(f'grid on\n')
+    f.write(f'pause')
+    f.close()
+    os.popen("octave plot3d.m")
+
+def plot_bar(labels, values):
+    values = [float(i) for i in values]
+    f = open("plot_bar.m", "w+")
+    f.write(f'bar({values})\n')
+    f.write(f'set(gca, "xtick", 1:{len(labels)}, "xticklabel", {labels})\n')
+    f.write(f'pause')
+    f.close()
+    os.popen("octave plot_bar.m")
+
+def plot_pie(labels, values):
+    values = [float(i) for i in values]
+    f = open("plot_pie.m", "w+")
+    f.write(f'pie({values})\n')
+    f.write(f'legend({labels})\n')
+    f.write(f'pause')
+    f.close()
+    os.popen("octave plot_pie.m")
+
 '''oc = Oct2Py()
 script = "plot(rand(1,10))"
 with open("myScript.m","w+") as f:
